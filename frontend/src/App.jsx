012f562091a6915e619c4fa1c5ea2372440af090
BrochotMaxime <brochot.max@gmail.com>
2026-06-03 22:43:07 +0200

    Fix frontend import paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Layout from '../src/components/Layout';
+import Layout from './components/layout';
 import ScrollToTop from './components/common/ScrollToTop';
 
 import HomePage from './pages/HomePage';
